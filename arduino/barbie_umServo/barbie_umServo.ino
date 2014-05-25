7432bd721399ba9089df12270e1aa8b65796f314
Otavio Carneiro <ocarneiro@outlook.com>
2014-05-25 19:41:38 -0300

    Codigo para controle de 3 servos

---
+++ arduino/barbie_umServo/barbie_umServo.ino
@@ -0,0 +1,41 @@
+//Servo controlado por dado recebido via Serial
+//Autor: Otavio Carneiro - 25/05/2014
+//Licenca: MIT - use como quiser, nao me responsabilize
+#include <Servo.h> 
+ 
+Servo barbie;  
+const int LED = 13;
+int valorRecebido = 0; //valor vindo de fora
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  barbie.attach(11); 
+} 
+ 
+void loop() 
+{
+  while (Serial.available()) // Wait for characters
+  {
+      int centena = Serial.read()-48;
+      Serial.println(centena);
+      int dezena = Serial.read()-48;
+      Serial.println(dezena);
+      int inteiro = Serial.read()-48;
+      Serial.println(inteiro);
+      valorRecebido = centena * 100 + dezena * 10 + inteiro;
+      Serial.println(valorRecebido);
+  }
+  if (valorRecebido == 100) {
+    digitalWrite(LED, HIGH);
+  } else if (valorRecebido == 0) {
+    digitalWrite(LED, LOW);
+  }
+  
+  int valorServo = map(valorRecebido, 0, 100, 20, 179);
+  barbie.write(valorServo);
+
+  delay(100);  
+}  
+
---
+++ arduino/painel_3servos/painel_3servos.ino
@@ -0,0 +1,56 @@
+//3 Servos controlados por dado recebido via Serial
+//Autor: Otavio Carneiro - 25/05/2014
+//Licenca: MIT - use como quiser, nao me responsabilize
+#include <Servo.h> 
+ 
+const int MIN_ENTRADA = 0;
+const int MAX_ENTRADA = 100;
+const int MIN_SERVO = 5;
+const int MAX_SERVO = 179;
+
+Servo servo1;  
+Servo servo2;  
+Servo servo3;  
+int valorRecebido1 = 0; //valores vindos de fora
+int valorRecebido2 = 0; //valores vindos de fora
+int valorRecebido3 = 0; //valores vindos de fora
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+  servo1.attach(9); 
+  servo2.attach(10); 
+  servo3.attach(11); 
+} 
+ 
+void loop() 
+{
+  while (Serial.available()) // Wait for characters
+  {
+      int centena = Serial.read()-48;
+      int dezena = Serial.read()-48;
+      int inteiro = Serial.read()-48;
+      valorRecebido1 = centena * 100 + dezena * 10 + inteiro;
+      Serial.println(valorRecebido1);
+      Serial.read(); //le o separador (virgula ou outro qualquer) e nao faz nada
+      centena = Serial.read()-48;
+      dezena = Serial.read()-48;
+      inteiro = Serial.read()-48;
+      valorRecebido2 = centena * 100 + dezena * 10 + inteiro;
+      Serial.println(valorRecebido2);
+      Serial.read(); //le o separador (virgula ou outro qualquer) e nao faz nada
+      centena = Serial.read()-48;
+      dezena = Serial.read()-48;
+      inteiro = Serial.read()-48;
+      valorRecebido3 = centena * 100 + dezena * 10 + inteiro;
+      Serial.println(valorRecebido3);
+      Serial.read(); //le o separador (virgula ou outro qualquer) e nao faz nada
+  }
+  
+  servo1.write(map(valorRecebido1, MIN_ENTRADA, MAX_ENTRADA, MIN_SERVO, MAX_SERVO));
+  servo2.write(map(valorRecebido2, MIN_ENTRADA, MAX_ENTRADA, MIN_SERVO, MAX_SERVO));
+  servo3.write(map(valorRecebido3, MIN_ENTRADA, MAX_ENTRADA, MIN_SERVO, MAX_SERVO));  
+  
+  delay(100);  
+}  
+
