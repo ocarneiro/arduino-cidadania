2cb6e4abfa28bba232fa8c6facba324163958885
Otavio Carneiro <ocarneiro@outlook.com>
2014-05-25 10:05:41 -0300

    Refactoring

--- serialCom.py
+++ serialCom.py
@@ -7,19 +7,12 @@ with serial.Serial(porta, velocidade) as conexao:
 
   estado = '0';
 
-  while estado == '0' or estado == '1' or estado == '23' :
+  while estado >= '0' and estado <= '100' :
+    print 'Enviando ', estado;
+    conexao.write(estado);
+    leitura_serial = conexao.readline();
+    print "Resposta = ", leitura_serial, "\n";
     estado = raw_input(" 1 acende \n 0 apaga \n 2 desliga \n");
-    print 'estado = ', estado;
-    # 0 e 1 vao para o arduino
-    if (estado >= '0' and estado <= '100'):
-        print 'enviando ', estado;
-        conexao.write(estado);
-        leitura_serial = conexao.readline();
-        print "Resposta = ", leitura_serial, "\n";
-    # 2 encerra
-    if (estado == '2'):
-        print 'ok, terminando';
-        break;
   print "Saindo"
 conexao.close();
 
