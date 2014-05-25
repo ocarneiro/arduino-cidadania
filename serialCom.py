6b414d5b6a0384d386face8f8e8e004039b0d416
Otavio Carneiro <ocarneiro@outlook.com>
2014-05-25 11:00:43 -0300

    Sleeps para melhorar comunicacao serial

--- serialCom.py
+++ serialCom.py
@@ -1,19 +1,24 @@
-import serial;
+import serial
+import time
 
-porta = "COM3";
-velocidade = 9600;
+porta = "COM3"
+velocidade = 9600
+debug = True
 
 with serial.Serial(porta, velocidade) as conexao:
 
-  estado = '0';
+  estado = '0'
+  time.sleep(2)
 
   while estado >= '0' and estado <= '100' :
-    print 'Enviando ', estado;
-    conexao.write(estado);
-    leitura_serial = conexao.readline();
-    print "Resposta = ", leitura_serial, "\n";
-    estado = raw_input(" 1 acende \n 0 apaga \n 2 desliga \n");
+    if debug: print 'Enviando ', estado
+    conexao.write(estado)
+    time.sleep(2) #espera o envio e a resposta
+    if debug: print 'Recebendo resposta'
+    leitura_serial = conexao.readline()
+    print "Resposta = ", leitura_serial, "\n"
+    estado = raw_input(" 1 acende \n 0 apaga \n 2 desliga \n")
   print "Saindo"
-conexao.close();
+conexao.close()
 
 
